5d7dd229c49fd157fe76045737d386643fb26cc2
salohiddin1221 <salohiddinkhayitov0@gmail.com>
2022-12-19 12:07:21 +0500

    en edn

--- routes/storage.js
+++ routes/storage.js
@@ -52,5 +52,15 @@ route.get('/:id', async (req, res) => {
     }
 })
 
+//delete storage
+route.delete('/:id', async (req, res) => {
+    try {
+        await Storage.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'Deleted' });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+})
+
 
 module.exports = route;
